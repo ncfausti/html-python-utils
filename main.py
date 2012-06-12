f9e5aee21e4f664aeebf4a0635aaf6d2f35b6b8f
Nick Fausti <ncfausti@gmail.com>
2012-06-12 00:26:56 -0400

    Adding main.py file. This is boiler plate for Google App Engine web applications.

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import string
+import cgi
+import re
+import os
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t= jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.response.out.write(self.render_str(template, **kw))
+
+class MainHandler(Handler):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+app = webapp2.WSGIApplication([('/unit3/blog/newpost', NewHandler), ('/unit3/blog', HomeHandler), (r'/unit3/blog/(\d+)', SingleHandler)], debug=True)
